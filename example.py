4b50b8f892c546b88ec4ca2371afabb3ee3d65a3
Malo G <malo@visoid.com>
2025-02-19 16:44:33 +0100

    Formatted files

--- example.py
+++ example.py
@@ -1,28 +1,47 @@
-from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler,ModelMixin, EulerAncestralDiscreteScheduler
+from diffusers import (
+    StableDiffusionControlNetPipeline,
+    ControlNetModel,
+    UniPCMultistepScheduler,
+    ModelMixin,
+    EulerAncestralDiscreteScheduler,
+)
 from PIL import Image
 from io import BytesIO
 from textEmbedder import text_embeddings
 import torch
 
 from MultiDiffusionPipeline import MultiStableDiffusion
-#this was never tested lol
-pipe = MultiStableDiffusion.from_pretrained("runwayml/stable-diffusion-v1-5",local_files_only=True, torch_dtype=torch.float16,safety_checker=None,requires_safety_checker=False).to("cuda")
-prompt=["a beautiful park","a beautiful sky"]
-negative_prompt=["a beautiful sky", "a beautiful park"]
 
-promptE=[]
-negative_promptE=[]
+# this was never tested lol
+pipe = MultiStableDiffusion.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    local_files_only=True,
+    torch_dtype=torch.float16,
+    safety_checker=None,
+    requires_safety_checker=False,
+).to("cuda")
+prompt = ["a beautiful park", "a beautiful sky"]
+negative_prompt = ["a beautiful sky", "a beautiful park"]
+
+promptE = []
+negative_promptE = []
 for i in range(len(prompt)):
-    cond, uncond =text_embeddings(pipe,prompt[i],"", clip_stop_at_last_layers=2)
+    cond, uncond = text_embeddings(pipe, prompt[i], "", clip_stop_at_last_layers=2)
     promptE.append(cond)
-    
-buffer=open('Untitled.png', 'rb')
+
+buffer = open("Untitled.png", "rb")
 buffer.seek(0)
 image_bytes = buffer.read()
 imageM = Image.open(BytesIO(image_bytes))
-pos=["0:0-512:512",imageM]#the first position should occupy the entirety of the picture size. it works the following: x0:y0-x1:y1 in pixels. the image passed is in black and white
-mask_types=[1,12] #the numbers here are essentially the equivalent of z-index in css less strength to background and more strength to mask.
-#loras_apply={1:["lora name in dir"]}  this applies the lora for the second area (the mask in this case)
+pos = [
+    "0:0-512:512",
+    imageM,
+]  # the first position should occupy the entirety of the picture size. it works the following: x0:y0-x1:y1 in pixels. the image passed is in black and white
+mask_types = [
+    1,
+    12,
+]  # the numbers here are essentially the equivalent of z-index in css less strength to background and more strength to mask.
+# loras_apply={1:["lora name in dir"]}  this applies the lora for the second area (the mask in this case)
 
 generator = torch.manual_seed(2733424006)
 # controlnet = ControlNetModel.from_pretrained("lllyasviel/control_v11p_sd15_openpose", local_files_only=True,torch_dtype=torch.float16).to("cuda")
